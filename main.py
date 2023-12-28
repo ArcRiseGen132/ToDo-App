fe8231b0ebd46c57cf238b550d8dae10875267e8
Sheikh_Ahmed <ssahme21@gmail.com>
2023-12-28 17:41:13 -0500

    Added edit feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,8 +10,16 @@ while True:
             todos.append(todo)
         case "show":
             for item in todos:
+                item = item.title()
                 print(item)
         case "exit":
             break
+        case "edit":
+            number = input("Number of the todo to edit: ")
+            existing_todo = int(todos[number - 1])
+            new_todo = input("Enter a new todo: ")
+            todos[existing_todo] = new_todo
+        case _:
+            print("Unknown command entered")
 
 print("Bye!")
