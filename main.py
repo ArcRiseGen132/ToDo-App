a0ba6b8714d662d9a310a4bf23d51ae487b2f001
Sheikh_Ahmed <ssahme21@gmail.com>
2023-12-30 21:31:20 -0500

    Added complete feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,16 +9,19 @@ while True:
             todo = input("Enter a todo:")
             todos.append(todo)
         case "show":
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{int(index) + 1}--{item.title()}"  # Add 1 to make numbering user-friendly
+                print(row)
         case "exit":
             break
         case "edit":
             number = input("Number of the todo to edit: ")
-            existing_todo = int(todos[number - 1])
+            existing_todo = int(todos[int(number) - 1])  # Subtract 1 for true array placement
             new_todo = input("Enter a new todo: ")
             todos[existing_todo] = new_todo
+        case "complete":
+            number = int(input("Number of the todo to complete or remove: "))
+            todos.pop(number - 1)  # Subtract 1 to find actual array placement
         case _:
             print("Unknown command entered")
 
